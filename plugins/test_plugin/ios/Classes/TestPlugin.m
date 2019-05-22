db2188fb2570def7a64bf0c419fe017e14c4fd0b
高昭亚 <gaozhaoya@videoup.tv>
2019-05-22 17:37:58 +0800

    DEL: test_plugin

--- plugins/test_plugin/ios/Classes/TestPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "TestPlugin.h"
-#import <test_plugin/test_plugin-Swift.h>
-
-@implementation TestPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftTestPlugin registerWithRegistrar:registrar];
-}
-@end
